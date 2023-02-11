222d64071f9ef751615f0491784bbeb4e7e47566
moriisaac <wesongamori@gmail.com>
2023-02-11 22:37:23 +0300

    Initial commit initial commit

--- jokes.py
+++
@@ -1,7 +0,0 @@
-# importing installed library
-import pyjokes
-
-
-stakrafyajokes = pyjokes.get_joke(language="en", category="all")
-
-print(stakrafyajokes)
--- matplotlibcrash.py
+++
@@ -1,8 +0,0 @@
-# basic line plot
-import matplotlib.pyplot as plt
-import numpy
-myarray = numpy.array([1, 2, 3])
-plt.plot(myarray)
-plt.xlabel('some x axis')
-plt.ylabel('some y axis')
-plt.show()
